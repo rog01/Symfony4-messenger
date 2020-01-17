9f679c637978d719e02007f918398ce421db3fbe
roger <rogerjohary@yahoo.fr>
2020-01-18 00:10:49 +0100

    Chapter 2: Message, Handler & the Bus

---
+++ src/Message/AddPonkaToImage.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: James
+ * Date: 18/01/2020
+ * Time: 00:03
+ */
+
+namespace App\Message;
+
+
+class AddPonkaToImage
+{
+
+}
---
+++ src/MessageHandler/AddPonkaToImageHandler.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: James
+ * Date: 18/01/2020
+ * Time: 00:04
+ */
+
+namespace App\MessageHandler;
+
+
+class AddPonkaToImageHandler
+{
+
+}
